ef8e2171725d2c5dba063cb1b821e88bd0c58950
Bianca Zimmer <bianca-katharina.zimmer@stud.uni-bamberg.de>
2022-04-19 16:58:40 +0200

    first version of finished code

--- sort_ttv.py
+++ sort_ttv.py
@@ -1,85 +1,145 @@
 # functions to sort images into train, test and validation folders
 
+import argparse
 import os
 import random
 import shutil
 
-base_dir = '/Users/biancazimmer/Documents/Masterthesis_data/data_keramy_small2'
+# sample usage:
+# python sort_ttv.py /Users/biancazimmer/Documents/Masterthesis_data/data_keramy_small2/train Users/biancazimmer/Documents/Masterthesis_data 0.2
 
-## functions
-def moveFileRename(file, pathfrom,  pathto):
-  if os.path.isfile(os.path.join(pathfrom,file)):
-    fileo = file
-    while os.path.isfile(os.path.join(pathto,file)):
-      file = str(random.sample([1,2,3,4,5,6,7,8,9,0], 1)).strip('[]') + file
-      print(fileo + " renamed to " + file)
-    os.rename(os.path.join(pathfrom,fileo), os.path.join(pathfrom,file))
-    shutil.move(os.path.join(pathfrom,file), os.path.join(pathto,file))
-  elif os.path.isfile(os.path.join(pathto,file)):
-    pass #file already moved
-  else:
-    print('File could not be moved: ' + os.path.join(pathfrom,file))
+# initialize parser for command line args
+parser = argparse.ArgumentParser()
+parser.add_argument("imagedir",
+                    help="Image directory in which images can be found. Images need to be sorted into folders by labels",
+                    type=str)
+parser.add_argument("basedir",
+                    help="Base directory into which images shall be sorted/folders should be created",
+                    type=str)
+parser.add_argument("testsplit",
+                    help="determins the test split. All_images * testsplit = size of test set",
+                    type=float)
+parser.add_argument("-v", "--validationset",
+                    help="adds a third folder for the validation set, else only the test set is split off",
+                    action="store_true")
+parser.add_argument("-s", "--validationsplit",
+                    help="determins the validation split. Traindata * validationsplit = size of validation set",
+                    action="store_true")
+args = parser.parse_args()
+
+
+# help functions
+
+
+def get_labels():
+    """Infers all the labels from the base directory"""
+    return [i for i in os.listdir(args.imagedir) if i != ".DS_Store"]  # TODO: there might be a nicer way to check this
+
+def makedirs():
+    """Makes directories for train, test and optional validation set"""
+    all_labels = get_labels()
+
+    train_dir = os.path.join(args.basedir, 'train')
+    testing_dir = os.path.join(args.basedir, 'test')
+
+    if args.validationset:
+        validation_dir = os.path.join(args.base_dir, 'val')
+
+    for l in all_labels:
+        os.makedirs(os.path.join(train_dir, l), exist_ok=True)
+        if args.validationset:
+            os.makedirs(os.path.join(validation_dir, l), exist_ok=True)
+        os.makedirs(os.path.join(testing_dir, l), exist_ok=True)
+
 
 def dim(a):
-  lengthdim = []
-  for elem in a:
-    lengthdim.append(len(elem))
-  return lengthdim
+    lengthdim = []
+    for elem in a:
+        lengthdim.append(len(elem))
+    return lengthdim
 
-def makedirs(perc_test = 0.2, perc_validation = 0.3, base_dir=base_dir):
-  listoffnames = []
-  test_fnames = []
-  val_fnames = []
-  train_fnames = []
-  all_features = [i for i in os.listdir(base_dir) if i not in ['Training', 'Testing', 'Validation', 'Models']]
 
-  train_dir = os.path.join(base_dir, 'Training')
-  validation_dir = os.path.join(base_dir, 'Validation')
-  testing_dir = os.path.join(base_dir, 'Testing')
-  for f in all_features:
-    os.makedirs(os.path.join(train_dir, f), exist_ok= True)
-    os.makedirs(os.path.join(validation_dir, f), exist_ok= True)
-    os.makedirs(os.path.join(testing_dir, f), exist_ok= True)
-    # get file names
-    fnames = [f for f in os.listdir(os.path.join(base_dir,f)) if f.endswith('.jpg')]
-    listoffnames.append(fnames)
-    #first put away testing data
-    test = random.sample(fnames, round(perc_test*len(fnames)))
-    test_fnames.append(test)
-    rest_fnames = list(set(fnames) - set(test))
-    # then define validation and training data
-    val = random.sample(rest_fnames, round(perc_validation*len(rest_fnames)))
-    val_fnames.append(val)
-    train = list(set(rest_fnames) - set(val))
-    train_fnames.append(train)
+def makeimagesplits(imagedir, perc_split=0.2):
+    """Splits the images from the imagedir into two splits"""
+    """Returns the current paths of the images as a list"""
 
-  print("Total number of all images: " + str(sum(dim(listoffnames))))
-  print(all_features)
-  print(dim(listoffnames))
-  print(['Training', 'Validation', 'Testing',])
-  print(str(sum(dim(train_fnames))) + ','  + str(sum(dim(val_fnames))) + ',' + str(sum(dim(test_fnames))))
-  return [train_fnames, val_fnames, test_fnames]
+    listoffnames = []
+    test_fnames = []
+    train_fnames = []
 
-def movefiles(train_fnames, val_fnames, test_fnames , base_dir=base_dir):
-  # move files into their respective directories
-  # training
-  for files, dir in zip(train_fnames, all_features):
-    for f in files:
-      moveFileRename(f, os.path.join(base_dir,dir),  os.path.join(base_dir,train_dir,dir))
+    for l in get_labels():
+        # get file names per label
+        fnames = [f for f in os.listdir(os.path.join(imagedir, l)) if f.endswith('.jpeg')]
+        listoffnames.append(fnames)
+        #print(fnames) # TODO
+        # first put away split off data
+        test = random.sample(fnames, round(perc_split * len(fnames)))
+        test_fnames.append(test)
+        rest_fnames = list(set(fnames) - set(test))
+        train_fnames.append(rest_fnames)
 
-  # validation
-  for files, dir in zip(val_fnames, all_features):
-    for f in files:
-      moveFileRename(f, os.path.join(base_dir,dir),  os.path.join(base_dir,validation_dir,dir))
+    print("Total number of all images: " + str(sum(dim(listoffnames))))
+    print(get_labels())
+    print(dim(listoffnames))
+    print(['Split1', 'Split2'])
+    print(str(sum(dim(train_fnames))) + ',' + str(sum(dim(test_fnames))))
+    return [train_fnames, test_fnames]
 
-  # testing
-  for files, dir in zip(test_fnames, all_features):
-    for f in files:
-      moveFileRename(f, os.path.join(base_dir,dir),  os.path.join(base_dir,testing_dir,dir))
+
+def copyfiles(train_fnames, imagefolder, destinationfolder):
+    """copy files into their respective directories"""
+    numcopied = []
+    for files, d in zip(train_fnames, get_labels()):
+        pathfrom = os.path.join(imagefolder, d)
+        pathto = os.path.join(destinationfolder, d)
+        for f in files:
+            if os.path.isfile(os.path.join(pathfrom, f)):
+                shutil.copy(os.path.join(pathfrom, f), os.path.join(pathto, f))
+        numcopied.append(len(os.listdir(pathto)))
+    return numcopied
+
+
+def movefiles(train_fnames, imagefolder, destinationfolder):
+    """copy files into their respective directories"""
+    nummoved = []
+    for files, d in zip(train_fnames, get_labels()):
+        pathfrom = os.path.join(imagefolder, d)
+        pathto = os.path.join(destinationfolder, d)
+        for f in files:
+            if os.path.isfile(os.path.join(pathfrom, f)):
+                shutil.move(os.path.join(pathfrom, f), os.path.join(pathto, f))
+        nummoved.append(len(os.listdir(pathto)))
+    return nummoved
+
+
+def checkmove(ntrain, ntest, nval=[0]):
+    numimg = []
+    for l in get_labels():
+        numimg.append(len(os.listdir(os.path.join(args.imagedir, l))))
+    print("pictures to move: " + str(sum(numimg)))
+    print("Train pictures: " + str(sum(ntrain)))
+    print("Test pictures: " + str(sum(ntest)))
+    print("Validation pictures: " + str(sum(nval)))
+
+
+# MAIN starts here
+makedirs()
+train_fnames, test_fnames = makeimagesplits(args.imagedir, args.testsplit)
+numtrain = copyfiles(train_fnames, args.imagedir, os.path.join(args.basedir, "train"))
+numtest = copyfiles(test_fnames, args.imagedir, os.path.join(args.basedir, "test"))
+
+if args.validationset:
+    train_fnames, val_fnames = makeimagesplits(os.path.join(args.basedir, "train"), args.validationsplit)
+    numval = movefiles(val_fnames, os.path.join(args.basedir, "train"), os.path.join(args.basedir, "val"))
+    checkmove(numtrain, numtest, numval)
+else:
+    checkmove(numtrain, numtest)
+
+# ----------------------------------
 
 
 # Get File Names and Make Directories
-# In order to set up the the training we need a certain file structure. For this wee need all out images devided into
+# In order to set up the  training we need a certain file structure. For this wee need all out images devided into
 # training, validation and test set (each a unique folder) and each again devided into the category folders.
 # This code takes all the folders in the base directory, from this it infers how many and which classes we have,
 # creates the folder structure as mentioned above and sorts the images accordingly. For this to be possible there
@@ -87,34 +147,3 @@ def movefiles(train_fnames, val_fnames, test_fnames , base_dir=base_dir):
 # and one folder per class.
 # **NOTE** If the directories already exist they will be updated but not over-written. So just put your new data in
 # the base_dir/class_folder and run this code chunk again.
-
-all_features = [i for i in os.listdir(base_dir) if i not in ['Training', 'Testing', 'Validation', 'Models']]
-train_fnames, val_fnames, test_fnames = makedirs()
-test_dir = os.path.join(base_dir, 'Testing')
-validation_dir = os.path.join(base_dir, 'Validation')
-train_dir = os.path.join(base_dir, 'Training')
-
-movefiles(train_fnames, val_fnames, test_fnames)
-
-# check if moving worked
-num_train_images = 0
-num_val_images = 0
-# overview stats
-print(all_features)
-for dir in ['Training', 'Validation', 'Testing']:
-    per_dir = []
-    for f in all_features:
-        # get file names
-        fnames = [f for f in os.listdir(os.path.join(base_dir, dir, f)) if f.endswith('.jpg')]
-        per_dir.append(len(fnames))
-    print(str(per_dir) + "   " + str(sum(per_dir)) + " - " + dir)
-    if dir == 'Training':
-        num_train_images = sum(per_dir)
-    if dir == 'Validation':
-        num_val_images = sum(per_dir)
-percentages = [round(i / sum(per_dir), 2) * 100 for i in per_dir]
-print(str(percentages) + "  - percentages  ")
-
-#
-# add an option to not make a validation directory -> only 2
-# add sys arguments to execute this from the console (for this: research sys arguments)
