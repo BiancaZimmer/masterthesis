821cd3a7e14369587abdf82993214be1a2a741cc
Bianca Zimmer <bianca-katharina.zimmer@stud.uni-bamberg.de>
2022-06-26 15:49:14 +0200

    started documentation of steps in the readme file changed the sort_ttv.py to be able to sort by patient ID

--- README.md
+++ README.md
@@ -3,11 +3,90 @@
 ### code_kermany
 Is the code downloaded from: XXX of the Paper of Kermany et. al. 2018. It
 is important that you use a Python 3.7 distribution with a Tensorflow 1.xx
-distribution here. Otherwise the code won't run.You can use the virtual
+distribution here. Otherwise the code won't run. You can use the virtual
 environment venv_py37 for this.
 
 You can run this code as describe in the extra README file under
 code_kermany > README.md
 
+### code_mh_main
+Is the modified code provided by Marvin Herchenbach et al. in the paper "XXXX Near Miss Near Hit"
+It came with a README.md file which I took the liberty to update and expand according to my needs.
+This is the main code I used for conducting my master thesis. The main changes are:
+1. Adding the ability to cope with a multi class data set (as opposed to the original binary classification)
+You have to set this ability in the utils.py file with the BINARY switch
+2. LRP
+3. Bounding Boxes
+
 ### sort_ttv.py
-Stand for sort test train validation
+Stands for sort test train validation. This is a helper Python file which can sort images
+into the correct folder structure. There is a little "How To" as a comment at the
+beginning of the file
+
+## Documentation:
+### 1. Sort images
+Sorted the images into the appropriate folders. 
+Data of each patient was either put into train, test OR validation split. </br>
+Parameters used in the makeimagesplits() function : `splitbypatient=True, maxiter=10, deviation=0.05)`
+
+Command used: </br>
+`python sort_ttv.py /Users/biancazimmer/Documents/Masterthesis_data/data_kermany/all /Users/biancazimmer/Documents/Masterthesis_data/data_kermany_split2 0.03 -s 0.1`
+
+This was used since in Original Dataset 250 images were used per class for testing ->
+that is at most 3% of the original size </br>
+For our training we thus use 0.03 testdata and 0.1 validation data
+
+Which resulted in the following output:
+```
+Cycle  0  failed. Proportion of  0  Trying again ...
+Cycle  1  failed. Proportion of  0.01716576532945893  Trying again ...
+Cycle  2  failed. Proportion of  0.02712083299701348  Trying again ...
+Winning proportion for  CNV :  0.03126429359377943
+Cycle  0  failed. Proportion of  0  Trying again ...
+Cycle  1  failed. Proportion of  0.023901243214848537  Trying again ...
+Cycle  2  failed. Proportion of  0.05235510418490632  Trying again ...
+Winning proportion for  DME :  0.03090527053055507
+Cycle  0  failed. Proportion of  0  Trying again ...
+Cycle  1  failed. Proportion of  0.041647331786542924  Trying again ...
+Cycle  2  failed. Proportion of  0.01786542923433875  Trying again ...
+Cycle  3  failed. Proportion of  0.020069605568445475  Trying again ...
+Cycle  4  failed. Proportion of  0.03306264501160093  Trying again ...
+Cycle  5  failed. Proportion of  0.022273781902552203  Trying again ...
+Cycle  6  failed. Proportion of  0.02679814385150812  Trying again ...
+Winning proportion for  DRUSEN :  0.030394431554524363
+Cycle  0  failed. Proportion of  0  Trying again ...
+Cycle  1  failed. Proportion of  0.03298288230159031  Trying again ...
+Cycle  2  failed. Proportion of  0.031768322769195736  Trying again ...
+Winning proportion for  NORMAL :  0.0285041940258853
+Total number of all images: 83556
+['CNV', 'DME', 'DRUSEN', 'NORMAL']
+[37167, 11422, 8620, 26347]
+['Split1', 'Split2']
+81028,2528
+Copying train files ...
+Copying test files ...
+Cycle  0  failed. Proportion of  0  Trying again ...
+Cycle  1  failed. Proportion of  0.1072906540758228  Trying again ...
+Winning proportion for  CNV :  0.10140258297458686
+Cycle  0  failed. Proportion of  0  Trying again ...
+Cycle  1  failed. Proportion of  0.09440780558316017  Trying again ...
+Cycle  2  failed. Proportion of  0.07408076610353238  Trying again ...
+Cycle  3  failed. Proportion of  0.0751648748757792  Trying again ...
+Winning proportion for  DME :  0.10145451260276447
+Cycle  0  failed. Proportion of  0  Trying again ...
+Winning proportion for  DRUSEN :  0.10134003350083752
+Cycle  0  failed. Proportion of  0  Trying again ...
+Winning proportion for  NORMAL :  0.09649945303953743
+Total number of all images: 81028
+['CNV', 'DME', 'DRUSEN', 'NORMAL']
+[36005, 11069, 8358, 25596]
+['Split1', 'Split2']
+72937,8091
+Moving validation files ...
+Pictures to move:  83556
+Train pictures:  72937
+Test pictures:  2528
+Validation pictures:  8091
+0h 29.0min 53.89623975753784sec
+```
+
--- sort_ttv.py
+++ sort_ttv.py
@@ -17,6 +17,7 @@ import argparse
 import os
 import random
 import shutil
+import numpy as np
 
 # initialize parser for command line args
 parser = argparse.ArgumentParser()
@@ -42,6 +43,7 @@ args = parser.parse_args()
 def get_labels():
     """Infers all the labels from the base directory"""
     return [i for i in os.listdir(args.imagedir) if i != ".DS_Store"]  # TODO: there might be a nicer way to check this
+    # return [i for i in os.listdir(args.imagedir) if os.path.isdir(i)]
 
 
 def makedirs():
@@ -68,14 +70,40 @@ def dim(a):
     return lengthdim
 
 
-def makeimagesplits(imagedir, perc_split=0.2):
+def split_by_patient(fnames, perc_split=0.2):
+    """ In the case of the Kermany et al data set we have multiple pictures per patient. Pictures of a patient should all be put into one split and not randomly assigned into both.
+
+    :param fnames: file names of the structure "XX-XX-XX"
+    :param perc_split: default=0.2
+    :return: Two list with the paths of the images in them
+    """
+    # split files at "-" and take the patient number
+    patientnumbers = [f.split("-")[1] for f in fnames]
+    set_of_patientnumbers = set(patientnumbers)
+    # sample from patient numbers
+    testpatients = random.sample(set_of_patientnumbers, round(perc_split * len(set_of_patientnumbers)))
+    # get files with sampled patient ID
+    idx_test = np.where(np.isin(patientnumbers, testpatients))[0]
+    idx_rest = np.where(np.isin(patientnumbers, testpatients, invert=True))[0]
+    return idx_test, idx_rest
+
+# python sort_ttv.py /Users/biancazimmer/Documents/Masterthesis_data/data_kermany_small3/train /Users/biancazimmer/Documents/Masterthesis_data 0.2
+
+
+def makeimagesplits(imagedir, perc_split=0.2, splitbypatient=False, maxiter=10, deviation=0.01):
     """Splits the images from the imagedir into two splits
     :param imagedir: directory where original images are
     :type imagedir: str
     :param perc_split: default=0.2
     :type perc_split: float
+    :param splitbypatient: In the case of the Kermany et al data set we have multiple pictures per patient. Pictures of a patient should all be put into one split and not randomly assigned into both.
+    :type splitbypatient: boolean
+    :param maxiter: maximum iteration when splitbypatient=True for resampling
+    :type maxiter: integer
+    :param deviation: maximum deviation from the perc_split when splitbypatient=True for resampling
+    :type deviation: float
 
-    :return: Current paths of the images as a list
+    :return: Two list with the paths of the images in them
     """
 
     listoffnames = []
@@ -86,12 +114,23 @@ def makeimagesplits(imagedir, perc_split=0.2):
         # get file names per label
         fnames = [f for f in os.listdir(os.path.join(imagedir, l)) if f.endswith('.jpeg')]
         listoffnames.append(fnames)
-        #print(fnames) # TODO
         # first put away split off data
         test = random.sample(fnames, round(perc_split * len(fnames)))
+        rest = list(set(fnames) - set(test))
+        if splitbypatient:
+            # while proportion is not right -> resample
+            proportion = 0
+            c = 0
+            while (perc_split*(1+deviation) < proportion or proportion < perc_split*(1-deviation)) and c < maxiter:
+                print("Cycle ", c, " failed. Proportion of ", proportion, " Trying again ...")
+                idx_test, idx_rest = split_by_patient(fnames, perc_split=perc_split)
+                proportion = len(idx_test)/len(fnames)
+                c = c+1
+            print("Winning proportion for ", l, ": ", proportion)
+            test = [np.array(fnames)[i] for i in idx_test]
+            rest = [np.array(fnames)[i] for i in idx_rest]
         test_fnames.append(test)
-        rest_fnames = list(set(fnames) - set(test))
-        train_fnames.append(rest_fnames)
+        train_fnames.append(rest)
 
     print("Total number of all images: " + str(sum(dim(listoffnames))))
     print(get_labels())
@@ -145,28 +184,45 @@ def movefiles(train_fnames, imagefolder, destinationfolder):
     return nummoved
 
 
-def checkmove(ntrain, ntest, nval=[0]):
+def checkmove():
     """Checks if the copy/move of the files was successful. Prints number of pictures in folders onto the console"""
     numimg = []
     for l in get_labels():
         numimg.append(len(os.listdir(os.path.join(args.imagedir, l))))
-    print("Pictures to move: " + str(sum(numimg)))
-    print("Train pictures: " + str(sum(ntrain)))
-    print("Test pictures: " + str(sum(ntest)))
-    print("Validation pictures: " + str(sum(nval)))
+    print("Pictures to move: ", sum(numimg))
+    numimg = []
+    for l in get_labels():
+        numimg.append(len(os.listdir(os.path.join(args.basedir, "train", l))))
+    print("Train pictures: ", sum(numimg))
+    numimg = []
+    for l in get_labels():
+        numimg.append(len(os.listdir(os.path.join(args.basedir, "test", l))))
+    print("Test pictures: ", sum(numimg))
+    if os.path.exists(os.path.join(args.basedir, "val")):
+        numimg = []
+        for l in get_labels():
+            numimg.append(len(os.listdir(os.path.join(args.basedir, "val", l))))
+        print("Validation pictures: ", sum(numimg))
 
 
 # MAIN starts here
 if __name__ == "__main__":
+    import time
+    tic = time.time()
     makedirs()
-    train_fnames, test_fnames = makeimagesplits(args.imagedir, args.testsplit)
+    train_fnames, test_fnames = makeimagesplits(args.imagedir, args.testsplit, splitbypatient=True, maxiter=10, deviation=0.05)
+    print("Copying train files ...")
     numtrain = copyfiles(train_fnames, args.imagedir, os.path.join(args.basedir, "train"))
+    print("Copying test files ...")
     numtest = copyfiles(test_fnames, args.imagedir, os.path.join(args.basedir, "test"))
 
     if args.validationsplit:
-        train_fnames, val_fnames = makeimagesplits(os.path.join(args.basedir, "train"), args.validationsplit)
+        train_fnames, val_fnames = makeimagesplits(os.path.join(args.basedir, "train"), args.validationsplit, splitbypatient=True, maxiter=10, deviation=0.05)
+        print("Moving validation files ...")
         numval = movefiles(val_fnames, os.path.join(args.basedir, "train"), os.path.join(args.basedir, "val"))
-        checkmove(numtrain, numtest, numval)
-    else:
-        checkmove(numtrain, numtest)
 
+    checkmove()
+
+    toc = time.time()
+    print("{}h {}min {}sec ".format(round(((toc - tic) / (60 * 60))), np.floor(((toc - tic) % (60 * 60)) / 60),
+                                    ((toc - tic) % 60)))
