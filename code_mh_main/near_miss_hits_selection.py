corrected Time calculation

code_mh_main/near_miss_hits_selection.py
@@ -380,7 +380,7 @@ def get_nhnm_overview(dataset, suffix_path="_multicnn", distance_measure='cosine
             get_nearest_miss_multi(rnd_img, data.available_classes, pred_label, data.data, fe, top_n, distance_measure)
 
     toc = time.time()
-    print("{}h {}min {}sec ".format(round(((toc - tic) / (60 * 60))), np.floor(((toc - tic) % (60 * 60)) / 60),
+    print("{}h {}min {}sec ".format(np.floor(((toc - tic) / (60 * 60))), np.floor(((toc - tic) % (60 * 60)) / 60),
                                     ((toc - tic) % 60)))
 
     # Show random image and its near misses and hits

code_mh_main/xai_demo.py
@@ -104,7 +104,7 @@ for dataset_name, embedding_dict in dict_datasets_and_embeddings.items():
 # use_CNN_feature_embedding = True
 
 toc = time.time()
-print("{}h {}min {}sec ".format(round(((toc - tic) / (60 * 60))), np.floor(((toc - tic) % (60 * 60)) / 60),
+print("{}h {}min {}sec ".format(math.floor(((toc - tic) / (60 * 60))), np.floor(((toc - tic) % (60 * 60)) / 60),
                                 ((toc - tic) % 60)))
 
 

sort_ttv.py
@@ -224,5 +224,5 @@ if __name__ == "__main__":
     checkmove()
 
     toc = time.time()
-    print("{}h {}min {}sec ".format(round(((toc - tic) / (60 * 60))), np.floor(((toc - tic) % (60 * 60)) / 60),
+    print("{}h {}min {}sec ".format(np.floor(((toc - tic) / (60 * 60))), np.floor(((toc - tic) % (60 * 60)) / 60),
                                     ((toc - tic) % 60)))