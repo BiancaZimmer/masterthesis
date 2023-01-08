73dbd0c8ecd2a2c2a5b09857333e55e486dc3acb
Bianca Zimmer <bianca-katharina.zimmer@stud.uni-bamberg.de>
2023-01-08 18:24:50 +0100

    ready for python notebook deleted nhnm function at the end

--- code_mh_main/jaccard_evaluation_final.py
+++ code_mh_main/jaccard_evaluation_final.py
@@ -91,7 +91,7 @@ def jaccard_df(df1, df2, method="intersection"):
 m0000_eucl = m0000_ssim = m0000_cw = []
 m0001_eucl = m0001_ssim = m0001_cw = []
 m0010_eucl = m0010_ssim = m0010_cw = []
-m0011_eucl = m0011_ssim = []  # m0011_cw = []
+m0011_eucl = m0011_ssim = m0011_cw = []
 m0100_eucl = m0101_eucl = []
 
 o1000_eucl = o1000_ssim = []
@@ -103,7 +103,7 @@ o1100_eucl = o1101_eucl = []
 all_df = {"0000_eucl": m0000_eucl, "0000_ssim": m0000_ssim, "0000_cw": m0000_cw,
           "0001_eucl": m0001_eucl, "0001_ssim": m0001_ssim, "0001_cw": m0001_cw,
           "0010_eucl": m0010_eucl, "0010_ssim": m0010_ssim, "0010_cw": m0010_cw,
-          "0011_eucl": m0011_eucl, "0011_ssim": m0011_ssim, # "0011_cw": m0011_cw,
+          "0011_eucl": m0011_eucl, "0011_ssim": m0011_ssim, "0011_cw": m0011_cw,
           "0100_eucl": m0100_eucl, "0101_eucl": m0101_eucl, 
           "1000_eucl": o1000_eucl, "1000_ssim": o1000_ssim,
           "1001_eucl": o1001_eucl, "1001_ssim": o1001_ssim,
@@ -115,7 +115,7 @@ all_df = {"0000_eucl": m0000_eucl, "0000_ssim": m0000_ssim, "0000_cw": m0000_cw,
 mnist_df_names = ["0000_eucl", "0000_ssim", "0000_cw",
           "0001_eucl", "0001_ssim", "0001_cw",
           "0010_eucl", "0010_ssim", "0010_cw",
-          "0011_eucl", "0011_ssim", # "0011_cw",
+          "0011_eucl", "0011_ssim", "0011_cw",
           "0100_eucl", "0101_eucl"]
 
 oct_df_names = ["1000_eucl", "1000_ssim",
@@ -128,7 +128,7 @@ oct_df_names = ["1000_eucl", "1000_ssim",
 # load all pickles
 for df in all_df:
     picklepath = "/Users/biancazimmer/Documents/PycharmProjects/masterthesis/code_mh_main/static/NHNM/" + df + \
-                 "_FINAL50.pickle"
+                 "_FINAL100.pickle"
     print(picklepath)
     all_df[df] = pd.read_pickle(picklepath)
 
@@ -148,9 +148,9 @@ for m in mnist_df:
     scores_top_names.append("scores_top_misses_"+m)
 
 # metrics
-eucl = [0, 3, 6, 9, 11, 12]
+eucl = [0, 3, 6, 9, 12, 13]
 ssim = [1, 4, 7, 10]
-cw = [2, 5, 8]
+cw = [2, 5, 8, 11]
 
 scores_top_names
 
@@ -440,6 +440,15 @@ for m1, df1 in oct_df.items():
 jaccards_oct
 # -
 
+# jaccards_oct.describe()
+jaccards_oct.groupby(["df1_name", "df2_name"]).describe()# .transpose()
+
+oct_df["1000_eucl"]["image_name"].equals(oct_df["1000_ssim"]["image_name"])
+
+jaccards_heatmap(jaccards_oct, "jaccard_misses")
+jaccards_heatmap(jaccards_oct, "jaccard_top_misses")
+jaccards_heatmap(jaccards_oct, "jaccard_hits")
+
 jaccards_oct.boxplot(column=["jaccard_misses", "jaccard_top_misses", "jaccard_hits"], by=["df1_name", "df2_name"],
                  figsize=(30, 30), rot=90, fontsize=20)
 
@@ -455,166 +464,5 @@ jaccards_oct.boxplot(column=["jaccard_misses_abs", "jaccard_top_misses_abs", "ja
 # * This is different to the mnist dataset
 
 plt.close("all")
-
-# ## Plot certain images + maps
-
-def plot_nhnm_overview_from_input(lst,
-                                  dataset, suffix_path="_multicnn", type_of_model="cnn", save_name='cosine',
-                                  top_n=TOP_N_NMNH, use_prediction=True, raw=False, distance_on_image=False
-                                  ):
-    """
-    list should have 
-    0 path to test image
-    1 list with paths to near hits
-    2 list with scores of near hits
-    3 list of lists with paths to near misses
-    4 list of lists with scores of near misses
-    Rest of input params like in near_miss_hits_selection.get_nhnm_overview()
-    """
-
-    from dataset import DataSet
-    from modelsetup import ModelSetup, load_model_from_folder, get_output_layer
-    from feature_extractor import FeatureExtractor
-    from near_miss_hits_selection import plot_nmnh, plot_nmnh_heatmaps
-
-    # Initialize Feature Extractor Instance
-    options_cnn = True if type_of_model == "cnn" else False
-    tmp_model = load_model_from_folder(dataset, suffix_path=suffix_path)
-    fe = FeatureExtractor(loaded_model=tmp_model,
-                          model_name=str.upper(type_of_model),
-                          options_cnn=options_cnn,
-                          feature_model_output_layer=get_output_layer(tmp_model, type_of_model))
-    data = DataSet(dataset, fe)
-
-    # if distance measure requires feature embedding check whether all are created and if not -> create them
-    if not distance_on_image:
-        i = 0  # Counter for newly loaded feature embeddings
-        for d in data.data:
-            if os.path.exists(d.feature_file):
-                np.load(d.feature_file, allow_pickle=True)
-                pass
-            else:  # if feature embedding doesn't exist yet, it is extracted now and saved
-                _, x = d.fe.load_preprocess_img(d.img_file)
-                feat = d.fe.extract_features(x)
-                np.save(d.feature_file, feat)
-                print("SAVE...")
-                i += 1
-
-        print("... newly loaded feature embeddings, which were not considered yet : ", i)
-
-    # initialize model
-    if type_of_model == "vgg":  # VGG16 will be used -> needs correct input shape # model_for_feature_embedding is None and
-        sel_model = ModelSetup(data, sel_size=224)
-    else:
-        sel_model = ModelSetup(data)
-    sel_model._preprocess_img_gen()
-    sel_model.set_model(suffix_path=suffix_path)
-
-    if type_of_model == 'cnn':
-        sel_model.mode_rgb = False
-    else:
-        sel_model.mode_rgb = True
-
-    test_img = DataEntry(fe, dataset, change_imgpath(lst[0]))
-    img, x = fe.load_preprocess_img(test_img.img_path)
-
-    pred_label = test_img.ground_truth_label
-
-    if use_prediction:
-        pred_label, pred_prob = sel_model.pred_test_img(test_img)
-        print("Ground Truth: ", test_img.ground_truth_label)
-        print("Prediction: ", pred_label)
-        print("Probability: ", pred_prob)
-
-    # near hits
-    scores_nearest_hit = lst[2]
-    ranked_nearest_hit_data_entry = [DataEntry(fe, dataset, change_imgpath(path)) for path in lst[1]]
-
-    # near misses
-    scores_nearest_miss_multi = lst[4]
-    ranked_nearest_miss_multi_data_entry = [[DataEntry(fe, dataset, change_imgpath(path)) for path in list2] for list2 in lst[3]]
-
-    # plot near hits and near misses
-    plt.ioff()
-    if top_n < 1:
-        print("SCORES NEAREST HITS")
-        print(pd.Series(scores_nearest_hit).describe())
-    else:
-        # Plot image + heatmap
-        heatmap_directory = os.path.join(STATIC_DIR, 'heatmaps', test_img.fe.fe_model.name, dataset)
-
-        fig1 = plt.figure()
-        plt.subplot(2, 1, 1)
-        plt.title(f"{test_img.img_name}\nActual Label : {test_img.ground_truth_label}\nPredicted Label : {pred_label}",
-                  weight='bold', size=10, loc="left")
-        plt.imshow(img, cmap='gray')
-        if distance_on_image and not raw:
-            # get correct path to heatmap
-            test_image_name = str.split(test_img.img_name, ".")[0]
-            test_image_heatmap_path = os.path.join(heatmap_directory, "test", pred_label[0],
-                                                   test_image_name + "_heatmap.png")
-            plt.subplot(2, 1, 2)
-            plt.title("Heatmap", weight='bold', size=12)
-            pic = cv2.imread(test_image_heatmap_path, cv2.IMREAD_GRAYSCALE)
-            plt.imshow(pic, cmap='gray', vmin=0, vmax=255)
-        plt.tight_layout()
-        plt.axis('off')
-        fig1.savefig("fig1.png", bbox_inches='tight')
-        plt.close(fig1)
-        # fig1.show()
-        # plt.show()
-
-        fig2 = plot_nmnh(ranked_nearest_hit_data_entry, scores_nearest_hit, title="Near Hits")
-        fig2.savefig("fig2.png", bbox_inches='tight')
-        plt.close()
-        if distance_on_image and not raw:
-            fig3 = plot_nmnh_heatmaps(ranked_nearest_hit_data_entry, scores_nearest_hit, pred_label[0],
-                                      title="Near Hits Heatmaps")
-            fig3.savefig("fig3.png", bbox_inches='tight')
-            plt.close()
-
-        fig4 = plot_nmnh(np.concatenate(ranked_nearest_miss_multi_data_entry),
-                         np.concatenate(scores_nearest_miss_multi),
-                         title="Near Misses per Class")
-        fig4.savefig("fig4.png", bbox_inches='tight')
-        plt.close()
-        if distance_on_image and not raw:
-            fig5 = plot_nmnh_heatmaps(np.concatenate(ranked_nearest_miss_multi_data_entry),
-                                      np.concatenate(scores_nearest_miss_multi), pred_label[0],
-                                      title="Near Misses Heatmaps per Class")
-            fig5.savefig("fig5.png", bbox_inches='tight')
-            plt.close()
-
-        # concatenate all pictures into one with cv2 and save it
-        # load pictures
-        pic1 = cv2.imread("fig1.png", cv2.IMREAD_GRAYSCALE)
-        pic2 = cv2.imread("fig2.png", cv2.IMREAD_GRAYSCALE)
-        try:
-            pic3 = cv2.imread("fig3.png", cv2.IMREAD_GRAYSCALE)
-            pic23 = cv2.hconcat([pic2, pic3])
-        except TypeError:  # FileNotFoundError
-            pic23 = pic2
-        pic4 = cv2.imread("fig4.png", cv2.IMREAD_GRAYSCALE)
-        try:
-            pic5 = cv2.imread("fig5.png", cv2.IMREAD_GRAYSCALE)
-            pic45 = cv2.hconcat([pic4, pic5])
-        except TypeError:  # FileNotFoundError
-            pic45 = pic4
-
-        # concatenate them
-        pic_all = hconcat_resize_max([pic1, vconcat_resize_min([pic23, pic45])])
-        save_path = STATIC_DIR+"/"+test_img.img_name+"_"+save_name+"_eval.png"
-        cv2.imwrite(save_path, pic_all)
-
-
-if __name__ == '__main__':
-    combination = "1010_eucl"
-
-    for i in range(0, 2):
-        plot_nhnm_overview_from_input(list(oct_df[combination].iloc[i]),
-                                          "oct_cc", suffix_path="_cnn_seed3871", type_of_model="cnn", save_name=combination,
-                                          top_n=TOP_N_NMNH, use_prediction=True, raw=False, distance_on_image=True)
-
-
 
 
